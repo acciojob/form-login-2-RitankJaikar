4e6b9aa5ac46a2d2d08634b293c7b24c287cbf15
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 11:51:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function submitForm() {
+  // Get values from the form
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const emailID = document.getElementById("emailID").value;
+
+  // Format and display the values in an alert
+  const alertMessage = 
+    `First Name: ${firstName} ` +
+    `Last Name: ${lastName} ` +
+    `Phone Number: ${phoneNumber} ` +
+    `Email ID: ${emailID}`;
+  
+  alert(alertMessage);
+}
